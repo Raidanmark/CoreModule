58e702f0b6b9ffc03e81ae38351e32555a98b02d
Raidanmark <Onidanil@Gmail.com>
2025-03-15 05:05:36 +0100

    Tickers are created

--- src/main/java/org/example/controllers/TickerController.java
+++ src/main/java/org/example/controllers/TickerController.java
@@ -1,4 +1,45 @@
 package org.example.controllers;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.example.dto.TickerDto;
+import org.example.exceptions.BadRequestException;
+import org.example.factories.TickerDtoFactory;
+import org.example.store.entities.TickerEntity;
+import org.example.store.repositories.TickerRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Transactional
+@RestController
 public class TickerController {
+    TickerRepository tickerRepository;
+
+    TickerDtoFactory tickerDtoFactory;
+
+    public static final  String CREATE_TICKER = "/api/tickers";
+
+    @PostMapping(CREATE_TICKER)
+    public TickerDto createTicker(@RequestParam String name, String symbol) {
+
+        tickerRepository
+                .findByName(name)
+                .ifPresent(ticker -> {
+                    throw new BadRequestException(String.format("Ticker \" %s\" already exists.", name));
+                });
+
+        TickerEntity ticker = tickerRepository.saveAndFlush(
+                TickerEntity.builder()
+                        .name(name)
+                        .symbol(symbol)
+                        .build()
+        );
+
+          return tickerDtoFactory.makeTickerDto(ticker);
+    }
 }
--- src/main/java/org/example/store/repositories/TickerRepository.java
+++ src/main/java/org/example/store/repositories/TickerRepository.java
@@ -3,5 +3,9 @@ package org.example.store.repositories;
 import org.example.store.entities.TickerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TickerRepository extends JpaRepository<TickerEntity, Long> {
+
+    Optional<TickerEntity> findByName(String name); 
 }
