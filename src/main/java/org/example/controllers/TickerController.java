6efeb73f4b310e814aab9ee36e91048beb19ba6e
Raidanmark <Onidanil@Gmail.com>
2025-03-21 07:45:09 +0100

    Added NotFoundException, Added fetch Tickers option to API

--- src/main/java/org/example/controllers/TickerController.java
+++ src/main/java/org/example/controllers/TickerController.java
@@ -5,13 +5,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.example.dto.TickerDto;
 import org.example.exceptions.BadRequestException;
+import org.example.exceptions.NotFoundException;
 import org.example.factories.TickerDtoFactory;
 import org.example.store.entities.TickerEntity;
 import org.example.store.repositories.TickerRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -23,9 +27,17 @@ public class TickerController {
     TickerDtoFactory tickerDtoFactory;
 
     public static final  String CREATE_TICKER = "/api/tickers";
+    public static final  String EDIT_TICKER = "/api/tickers/{ticker_id}";
+    public static final  String FETCH_TICKERS = "/api/tickers";
+
 
     @PostMapping(CREATE_TICKER)
     public TickerDto createTicker(@RequestParam String name, String symbol) {
+
+        //Name & symbol validation
+        if (name.trim().isEmpty() || symbol.trim().isEmpty()) {
+            throw new BadRequestException("Ticker name or symbol cannot be empty");
+        }
 
         tickerRepository
                 .findByName(name)
@@ -41,5 +53,48 @@ public class TickerController {
         );
 
           return tickerDtoFactory.makeTickerDto(ticker);
+    }
+
+    @PatchMapping(EDIT_TICKER)
+    public TickerDto editPatch(
+            @PathVariable("ticker_id") String tickerId,
+            @RequestParam String name, @RequestParam String symbol) {
+
+        if (name.trim().isEmpty() || symbol.trim().isEmpty()) {
+            throw new BadRequestException("Ticker name or symbol cannot be empty");
+        }
+
+        TickerEntity ticker = tickerRepository
+                .findById(Long.valueOf(tickerId))
+                .orElseThrow(() ->
+                        new NotFoundException(String.format(
+                                "Ticker with \" %s \" doesn't exist.",
+                                tickerId
+                        ))
+                );
+
+        tickerRepository
+                .findByName(name)
+                .filter(anotherTicker -> !Objects.equals(anotherTicker.getName(), name))
+                .filter(anotherTicker -> !Objects.equals(anotherTicker.getSymbol(), symbol))
+                .ifPresent(anotherTicker -> {
+                    throw new BadRequestException(String.format(
+                            "Ticker \" %s\" already exists.",
+                            name
+                    ));
+                });
+
+        ticker.setName(name);
+        ticker.setSymbol(symbol);
+
+        ticker = tickerRepository.saveAndFlush(ticker);
+        return tickerDtoFactory.makeTickerDto(ticker);
+    }
+
+    @GetMapping(FETCH_TICKERS)
+    public List<TickerDto> fetchTickers() {
+        return tickerRepository.streamAllBy()
+                .map(tickerDtoFactory::makeTickerDto)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/org/example/store/repositories/TickerRepository.java
+++ src/main/java/org/example/store/repositories/TickerRepository.java
@@ -4,8 +4,10 @@ import org.example.store.entities.TickerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public interface TickerRepository extends JpaRepository<TickerEntity, Long> {
 
-    Optional<TickerEntity> findByName(String name); 
+    Optional<TickerEntity> findByName(String name);
+    Stream<TickerEntity> streamAllBy();
 }
